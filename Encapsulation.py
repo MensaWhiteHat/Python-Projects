097db9cee6b063078b5631827746e0459c81cb2b
John Doe <samspinda@gmail.com>
2020-06-27 01:32:24 -0600

    Push for Assignment

--- Encapsulation.py
+++ Encapsulation.py
@@ -16,3 +16,20 @@ user.displayName()
 user.name = "The Tech Academy"
 user.displayName()
 print(user._password)
+
+
+
+
+
+#abc_base.py
+
+import abc
+class PluginBase(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def load(self, input):
+        """Retrieve data as input source
+         and return an object"""
+    @abc.abstractmethod
+    def save(self, output, data):
+        """save data ibject to that output"""
